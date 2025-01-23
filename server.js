6a2afcadd0b8af41c0c2d6b278882e2a446ad373
Chovanec <Dominik.Chovanec@magna.com>
2025-01-23 06:47:16 +0100

    Addind a SSL

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const Pool = require('pg').Pool;
+const https = require('https')
+const fs = require('fs')
 require('dotenv').config();
 const booksRouter = require('./src/routes/booksRouter');
 
@@ -9,6 +11,10 @@ const booksRouter = require('./src/routes/booksRouter');
 
 const app = express();
 const port = process.env.PORT_BACKEND
+const secureOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/dodko.site/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/dodko.site/fullchain.pem')
+}
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,6 +29,6 @@ app.use('/api/books', booksRouter);
 app.get('*', (req, res) => {
     res.status(404).send({message: `Stranka ${req.originalUrl} neexistuje`});
 })
-app.listen(port, () => {
+app.listen(secureOptions,port, () => {
     console.log(`My Book management application listen on port: ${port}`);
 });
